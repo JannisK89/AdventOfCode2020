5ae967421464496d2fe24973121516139be07ea4
JannisK <Jannis.Karanikis1@gmail.com>
2020-12-21 17:51:46 +0100

    Day5

---
+++ Day5/part1.py
@@ -0,0 +1,43 @@
+
+# https://adventofcode.com/2020/day/5
+
+
+def findRow(file):
+    listOfRows = [x for x in range(0, 128)]
+
+    for i in range(0, 7):
+        if file[i] == 'F':
+            listOfRows = listOfRows[0:int(len(listOfRows)/2)]
+
+        else:
+            listOfRows = listOfRows[int(len(listOfRows)/2):]
+
+    return listOfRows[0]
+
+
+def findColumn(file):
+    listOfColumns = [x for x in range(0, 8)]
+
+    for i in range(7, 10):
+        if file[i] == 'L':
+            listOfColumns = listOfColumns[0:int(len(listOfColumns)/2)]
+
+        else:
+            listOfColumns = listOfColumns[int(len(listOfColumns)/2):]
+
+    return listOfColumns[0]
+
+
+def getSeatIds(file):
+
+    fileInput = open(file, 'r').readlines()
+    listOfIds = []
+
+    for line in fileInput:
+        listOfIds.append(findRow(line.strip()) * 8 + findColumn(line.strip()))
+
+    return listOfIds
+
+
+print(max(getSeatIds('input.txt'))) 
+
