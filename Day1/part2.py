b782b005056c055d03f23be4a7e435004c7ff7e9
JannisK <Jannis.Karanikis1@gmail.com>
2020-12-21 17:59:12 +0100

    Minor code clean up

--- Day1/part2.py
+++ Day1/part2.py
@@ -1,15 +1,8 @@
-"""
---- Part Two ---
-The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. 
-They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
 
-Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
-
-In your expense report, what is the product of the three entries that sum to 2020?
-"""
+# # https://adventofcode.com/2020/day/1
 
 
-def FindAndMultiply(file):
+def findAndMultiply(file):
     inputList = open(file, 'r').readlines()
 
     for firstNumber in inputList:
@@ -20,4 +13,4 @@ def FindAndMultiply(file):
                         return int(firstNumber) * int(secondNumber) * int(thirdNumber)
 
 
-print(FindAndMultiply("input.txt"))
+print(findAndMultiply("input.txt"))
--- Day2/part1.py
+++ Day2/part1.py
@@ -1,32 +1,14 @@
-"""
---- Day 2: Password Philosophy ---
-Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
 
-The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+# https://adventofcode.com/2020/day/2
 
-Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
 
-To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
-
-For example, suppose you have the following list:
-
-1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc
-Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
-
-In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
-
-How many passwords are valid according to their policies?
-
-"""
 import re
 
 
-def ValidatePassword(inputLine):
+def validatePassword(inputLine):
 
     rangeTuple = (int(inputLine[0:inputLine.index('-')]),
-                     int(inputLine[inputLine.index('-')+1:inputLine.index(' ')]))
+                  int(inputLine[inputLine.index('-')+1:inputLine.index(' ')]))
 
     letterToCheck = inputLine[inputLine.index(' ')+1:inputLine.index(':')]
 
@@ -40,15 +22,15 @@ def ValidatePassword(inputLine):
     return False
 
 
-def FindValidPasswords(file):
+def findValidPasswords(file):
     ValidPasswords = 0
     passwords = open(file, 'r').readlines()
 
     for password in passwords:
-        if ValidatePassword(password):
+        if validatePassword(password):
             ValidPasswords += 1
-        
+
     return ValidPasswords
 
 
-print(FindValidPasswords('input.txt'))
+print(findValidPasswords('input.txt'))
